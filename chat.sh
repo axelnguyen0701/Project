59f73bb035796aeaa2022f255dfa12de85080564
Axel Nguyen <nguyent283@mymacewan.ca>
2020-11-29 16:23:51 -0700

    melfort-hear now can print INDIVIDUAL user's texts

--- chat.sh
+++ chat.sh
@@ -17,29 +17,39 @@ function melfort-say {
 function melfort-hear {
     #Put all path to users' messages to  an array all_messages
     all_messages=($(find $MELFORT_ROOT -name messages 2>/dev/null | xargs echo))
-    declare -a found_users
+    declare -A users_messages
 
     #Format elements in the array:
     for i in ${all_messages[@]};
-    do 
-        i=${i%/chat/messages};
-        i=${i#/var/melfort/};
-        found_users+=$i;
-    done;
-    echo ${found_users[@]}
+    do
+        path=$i;
+        name=${i%/chat/messages};
+        name=${name#/var/melfort/};
+        users_messages[$name]="$path";
+    done;   
+
     #Prompt for user's message
     PS3="Choose user's messages you want to hear "
-    select USER_MESSAGE in "${found_users[@]}" STOP
+    select USER_NAME in "${!users_messages[@]}" STOP
     do
-        if [ "$USER_MESSAGE" == "" ] 
+        if [ "$USER_NAME" == "" ] 
             then
                 echo -e "Invalid entry.\n"
                 continue
-        elif [ "$USER_MESSAGE" == STOP ]
+        elif [ "$USER_NAME" == STOP ]
             then    
                 echo "See ya!"
                 break
         fi
-    cat "$MELFORT_ROOT/$USER_MESSAGE/chat/messages"
+#format output using awk
+#UNIX to date: https://stackoverflow.com/questions/31476602/unix-time-to-date-and-replace-in-bash-using-awk
+#Skip to the rest: https://www.cyberciti.biz/faq/unix-linux-bsd-appleosx-skip-fields-command/
+        awk -v user=$USER_NAME ' {
+            $1 = strftime("%d-%m-%Y %H:%M:%S",$1);
+            printf "%s at %s said: ", user, $1;
+            $1="";
+            print;
+            }
+            ' ${users_messages[$USER_NAME]}
     done
 }
